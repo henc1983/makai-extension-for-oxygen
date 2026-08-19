f98494778827d69b96c84681c5ed8448a1e92e8a
Makai Henrik <makaihenrik@Makai-MacBook-Pro.local>
2026-08-19 19:32:02 +0200

    Create update.php

--- makai-extension.php
+++ makai-extension.php
@@ -55,3 +55,4 @@ require_once MEX_PLUGIN_DIR . '/inc/enqueue.php';
 require_once MEX_PLUGIN_DIR . '/inc/ajax_handlers.php';
 require_once MEX_PLUGIN_DIR . '/inc/elements_register.php';
 require_once MEX_PLUGIN_DIR . '/inc/reusable_dependencies.php';
+require_once MEX_PLUGIN_DIR . '/inc/update.php';
