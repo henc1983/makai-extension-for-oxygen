d050f1c3d7851ca658f06ee5bc5419fd35a4fabd
Makai Henrik <henc19830328@gmail.com>
2026-08-20 10:20:29 +0200

    Translated on Forms and append newsletter form

--- oxygen_builder/elements/SubscribeNewsForm/element.php
+++ oxygen_builder/elements/SubscribeNewsForm/element.php
@@ -7,20 +7,20 @@ use function Breakdance\Elements\PresetSections\getPresetSection;
 
 
 \Breakdance\ElementStudio\registerElementForEditing(
-    "MakaiExtensions\\Withrawalform",
+    "MakaiExtensions\\Subscribenewsform",
     \Breakdance\Util\getdirectoryPathRelativeToPluginFolder(__DIR__)
 );
 
-class Withrawalform extends \Breakdance\Elements\Element
+class Subscribenewsform extends \Breakdance\Elements\Element
 {
     static function uiIcon()
     {
-        return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M480-40q-112 0-206-51T120-227v107H40v-240h240v80h-99q48 72 126.5 116T480-120q75 0 140.5-28.5t114-77q48.5-48.5 77-114T840-480h80q0 91-34.5 171T791-169q-60 60-140 94.5T480-40Zm-36-160v-52q-47-11-76.5-40.5T324-370l66-26q12 41 37.5 61.5T486-314q33 0 56.5-15.5T566-378q0-29-24.5-47T454-466q-59-21-86.5-50T340-592q0-41 28.5-74.5T446-710v-50h70v50q36 3 65.5 29t40.5 61l-64 26q-8-23-26-38.5T482-648q-35 0-53.5 15T410-592q0 26 23 41t83 35q72 26 96 61t24 77q0 29-10 51t-26.5 37.5Q583-274 561-264.5T514-250v50h-70ZM40-480q0-91 34.5-171T169-791q60-60 140-94.5T480-920q112 0 206 51t154 136v-107h80v240H680v-80h99q-48-72-126.5-116T480-840q-75 0-140.5 28.5t-114 77q-48.5 48.5-77 114T120-480H40Z"/></svg>';
+        return 'SquareIcon';
     }
 
     static function tag()
     {
-        return 'section';
+        return 'div';
     }
 
     static function tagOptions()
@@ -35,12 +35,12 @@ class Withrawalform extends \Breakdance\Elements\Element
 
     static function name()
     {
-        return 'Withrawal Form';
+        return 'Subscribe News Form';
     }
 
     static function className()
     {
-        return 'mex-withrawal';
+        return 'mex-subscribe-news-form';
     }
 
     static function category()
@@ -101,7 +101,7 @@ class Withrawalform extends \Breakdance\Elements\Element
 
     static function dependencies()
     {
-        return ['0' =>  ['scripts' => ['%%BREAKDANCE_REUSABLE_MEXWITHRAWAL%%','%%BREAKDANCE_REUSABLE_MEXFORMS%%'],'styles' => ['%%BREAKDANCE_REUSABLE_MEXFORMSCSS%%'],],];
+        return ['0' =>  ['scripts' => ['%%BREAKDANCE_REUSABLE_MEXFORMS%%'],'styles' => ['%%BREAKDANCE_REUSABLE_MEXFORMSCSS%%'],],];
     }
 
     static function settings()
@@ -157,7 +157,7 @@ class Withrawalform extends \Breakdance\Elements\Element
 
     static function additionalClasses()
     {
-        return [['name' => 'form-wrapper', 'template' => 'no']];
+        return [['name' => 'form-wrapper', 'template' => 'no'], ['name' => 'small', 'template' => 'no']];
     }
 
     static function projectManagement()
